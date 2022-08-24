a571cfb711166aad0697da67852e0fa0f2f7d93a
Hcps-ahsansm1 <112025760+Hcps-ahsansm1@users.noreply.github.com>
2022-08-24 18:28:54 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+/********************************************************************
+*Hello.java
+/********************************************************************
+*Maira Ahsan
+*8/24/2022
+*CMSC-255-001
+********************************************************************/
+
+public class Hello {
+	public static void main (String [] args) {
+	
+		System.out.println("Maira Ahsan");
+		System.out.println("Hello.java");
+		System.out.println("prints to the command line");
+		System.out.println("8/24/2022");
+		System.out.println("CMSC-255-001");
+		System.out.println();
+
+	}
+}	
+
+
+
